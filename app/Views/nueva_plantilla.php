b9af4a804899f3b0ba3b723a09d4b52ba8bb6994
mauri <lezanamauricio86@gmail.com>
2023-04-05 20:11:41 -0300

    first commit

---
+++ app/Views/nueva_plantilla.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="" crossorigin="">
+    <title>Zapateria Lezana</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <section class="container-fluid px-0  sticky-md-top ">
+        <nav class="row navbar px-0 mx-0 navbar-expand-sm bg-dark navbar-dark ">
+        <div class="col-sm-2 col-lg-1 ps-2 container pe-0 mx-0">
+				<a class="navbar-brand" href="#">Logo</a>
+			</div>
+			<div class="container col-sm-2 col-lg-8 ps-0 navbar-header text-primary ">ZAPATILLAS LEZANA</div>
+		    <div class="col-sm-8 col-lg-3 container-fluid">
+			    <ul class="navbar-nav">
+					<li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+					<li class="nav-item"><a class="nav-link" href="#">Products</a></li>
+				    <li class="nav-item"><a class="nav-link " href="#">Brands</a></li>
+			    </ul>
+			</div>
+        </nav>
+    </section>
+    <section class="container-fluid px-0 h-50">
+        <!-- Carousel -->
+        <div id="demo" class="carousel slide h-100 " data-bs-ride="carousel">
+
+            <!-- Indicators/dots -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+            </div>
+
+            <!-- The slideshow/carousel -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/img/img_carousel/primerImg.jpg" alt="zapatilla1" class="d-block w-75 mx-auto">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/img_carousel/segundaImg.jpg" alt="zapatilla2" class="d-block w-75 mx-auto">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img/img_carousel/terceraImg.jpg" alt="zapatilla3" class="d-block w-75 mx-auto">
+                </div>
+            </div>
+
+            <!-- Left and right controls/icons -->
+            <button class="carousel-control-prev bg-dark" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next bg-dark" type="button" data-bs-target="#demo" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </button>
+        </div>
+    </section>
+    <div>
+        <p class="h2">Zapateria Lezana</p>
+    </div>
+    <section class="container m-3">
+        <p class="text-center fw-bold text-uppercase fsc-italic">El mejor lugar para comprar zapatos</p>
+        <p>tenemos muchas variedades de zapatos al mejor precio del mercado para todo tipo de zapato, podes hacer tus pedidos online y recibirlos en las puerta de tu casas o venir a nuestros locales </p>
+        <p>Estamos en Buenos Aires,Corrientes,Cacho Y Misiones. Somos una empresa en crecimiento y nos gustaria expandirnos al resto de Argentina </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, iure eaque incidunt iste accusamus error quos nemo ad tenetur qui ex ratione laboriosam. Sunt accusantium corporis laboriosam tempore deleniti alias.</p>
+    </section>
+    <div class="container-fluid mt-5 px-0 fixed-bottomrow ">
+		<!-- Footer -->
+		<footer
+				class="text-center text-lg-start text-white"
+				style="background-color: #45526e"
+				>
+		  <!-- Grid container -->
+		  <div class="container-fluid p-0 pb-0">
+			<!-- Section: Links -->
+			<section class="px-0">
+			  <!--Grid row-->
+			  <div class="container row "> 
+				<!-- Grid column -->
+				<div class="col-md-3 col-lg-2 col-xl-3 mx-auto mt-3 ">
+				  <h6 class="text-uppercase mb-4 font-weight-bold">
+					Who we are?
+				  </h6>
+				  <p>
+					we're a company who is willing to give our costumers the best comfor at the best prices.
+					There are tons steps to make,why dont start with a good shoe?
+					 
+				  </p>
+				</div>
+				<!-- Grid column -->
+	  
+				<hr class="w-100 clearfix d-md-none" />
+	  
+				<!-- Grid column 
+				<div class="col-md-2 col-lg-2 col-xl-3 mx-auto mt-3">
+				  <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+				  <p>
+					<a class="text-white">MDBootstrap</a>
+				  </p>
+				  <p>
+					<a class="text-white">MDWordPress</a>
+				  </p>
+				  <p>
+					<a class="text-white">BrandFlow</a>
+				  </p>
+				  <p>
+					<a class="text-white">Bootstrap Angular</a>
+				  </p>
+				</div>
+				<!-- Grid column -->
+	  
+				<hr class="w-100 clearfix d-md-none" />
+	  
+				<!-- Grid column -->
+				<div class="col-md-3 col-lg-2 col-xl-3 mx-auto mt-3">
+				  <h6 class="text-uppercase mb-4 font-weight-bold">
+					Useful links
+				  </h6>
+				  <p>
+					<a class="text-white">Your Account</a>
+				  </p>
+				  <p>
+					<a class="text-white">Become an Affiliate</a>
+				  </p>
+				  <p>
+					<a class="text-white">Shipping Rates</a>
+				  </p>
+				  <p>
+					<a class="text-white">Help</a>
+				  </p>
+				</div>
+	  
+				<!-- Grid column -->
+				<hr class="w-100 clearfix d-md-none" />
+	  
+				<!-- Grid column -->
+				<div class="col-md-4 col-lg-3 col-xl-3 mx-auto px-0 mt-3">
+				  <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+				  <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+				  <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+				  <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+				  <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+				</div>
+				<!-- Grid column -->
+				<!-- Grid column -->
+				  <div class="col-md-4 col-lg-3 col-xl-3 mx-auto px-0 mt-3">
+				  <h6 class="text-uppercase mb-4 font-weight-bold">where to find us</h6>
+				  <p><i class="fas fa-home mr-3"></i> Corrientes,Corrientes, Junin 750,AR</p>
+				  <p><i class="fas fa-home mr-3"></i> Chaco, Resistencia Sarmiento 893, AR</p
+				  <p><i class="fas fa-home mr-3"></i> Buenos Aires,La Plata, calle falsa 150, AR</p
+				  <p><i class="fas fa-home mr-3"></i> Misiones, Iguazu, calle principal 300, AR</p
+				</div>
+				<!-- Grid column -->
+			  </div>
+			  <!--Grid row-->
+			</section>
+			<!-- Section: Links -->
+	  
+
+			  </div>
+			</section>
+			<!-- Section: Copyright -->
+		  </div>
+		  <!-- Grid container -->
+		</footer>
+		<!-- Footer -->
+	  </div> 
+</body>
+</html>
